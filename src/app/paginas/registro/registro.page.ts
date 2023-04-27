16b7705cafe22c2f3657da7757493aa0dce41c6f
Técnico de Informática 2021.1 <39127512021.1@senacrjedu.local>
2023-04-26 21:10:10 -0300

    Registro de usuários

--- src/app/paginas/registro/registro.page.ts
+++ src/app/paginas/registro/registro.page.ts
@@ -1,20 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AlertController, IonicModule, NavController } from '@ionic/angular';
+import { AutenticacaoService } from 'src/app/servicos/autenticacao.service';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule
+    , ReactiveFormsModule]
 })
 export class RegistroPage implements OnInit {
-
-  constructor() { }
+  credencial!: FormGroup;
+  constructor(private service: AutenticacaoService,
+              private fb: FormBuilder,
+              private nav: NavController,
+              private alerta: AlertController) { }
 
   ngOnInit() {
+    this.credencial = this.fb.group({
+      email: ['', [Validators.required, 
+                   Validators.email]],
+      senha: ['', [Validators.required,
+                   Validators.minLength(6)]]
+    });
   }
 
+  
+  async registrar(){
+    console.log(this.credencial.get('email')?.value);
+    const user = await this.service.registrar(
+      this.credencial.get('email')?.value, 
+      this.credencial.get('senha')?.value);
+    if (user){
+      this.nav.navigateForward("interno");
+    } else {
+      console.log("Erro");
+    } 
+  }
 }
--- src/app/servicos/autenticacao.service.ts
+++ src/app/servicos/autenticacao.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword } 
+  from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutenticacaoService {
 
-  constructor() { }
+  constructor(private auth: Auth) { }
+
+  registrar(email:any, senha:any){
+    console.log("Email: " + email);
+    console.log("Senha: " + senha);
+    const usuario = createUserWithEmailAndPassword(
+      this.auth, email, senha);
+    return usuario;  
+  }
 }
